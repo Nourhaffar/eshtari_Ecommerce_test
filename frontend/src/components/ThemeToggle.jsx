4512a476eb38ee5e09e83778af5be1e80beba849
NourHaffar <nourcraftmodded@gmail.com>
2025-12-15 17:28:43 +0200

    edit the navbar  by using lucide lib for the icon

--- frontend/src/components/ThemeToggle.jsx
+++ frontend/src/components/ThemeToggle.jsx
@@ -1,30 +1,14 @@
 import { useTheme } from "../contexts/ThemeContextBase";
+import { Sun, Moon } from "lucide-react";
 
 const ThemeToggle = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <label className="swap swap-rotate btn btn-ghost btn-circle">
-      {/* this hidden checkbox controls the state */}
       <input type="checkbox" onChange={toggleTheme} checked={theme === "dark"} />
-
-      {/* sun icon */}
-      <svg
-        className="swap-on fill-current w-6 h-6"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-      >
-        <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,5.64,7.05Zm12,1.41a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,17.65,7.05ZM17.65,17l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,17.65,17ZM12,19a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A10,10,0,1,0,5.64,7,11.58,11.58,0,0,0,18.36,17Z" />
-      </svg>
-
-      {/* moon icon */}
-      <svg
-        className="swap-off fill-current w-6 h-6"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-      >
-        <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-      </svg>
+      <Sun className="swap-on fill-current w-6 h-6" />
+      <Moon className="swap-off fill-current w-6 h-6" />
     </label>
   );
 };
